d9bb19e636420a4f741c1bb131ee8d7b0063801c
wuchangyun <cywu@streamax.com>
2025-05-17 17:26:37 +0800

    修改描述：[feat] 部分内容改造 影响分析： 是否自测： 自测内容： 测试建议： 代码审查： NOTE： 项目编号

--- blog/rspress.config.ts
+++ blog/rspress.config.ts
@@ -74,6 +74,30 @@ export default defineConfig({
         text: '友情链接',
         link: '/blog/flinks/',
         activeMatch: '/blog/flinks/',
+        items: [
+          {
+            text: 'GitHub',
+            link: 'http://github.com/',
+          },
+          {
+            text: 'X',
+            link: 'http://x.com/',
+          },
+          // 也可以是一个导航栏组
+          {
+            text: 'Group',
+            items: [
+              {
+                text: 'Personal',
+                link: 'http://example.com/',
+              },
+              {
+                text: 'Company',
+                link: 'http://example.com/',
+              },
+            ],
+          },
+        ]
       },
       {
         text: '归档',
